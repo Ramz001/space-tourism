860ea1c92dab2cd372ac49e6ef661118762c6eb0
Ramz001 <rkenjaev1@gmail.com>
2022-05-11 11:34:14 +0500

    404 fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/space-tourism'>
       <Provider store={store}>
         <App />
       </Provider>
